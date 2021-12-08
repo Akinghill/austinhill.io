6f57e671367f81dcec1da382fe60bf3f72eaa4fa
akinghill <austinkinghill@gmail.com>
2021-12-07 22:51:49 -0700

    lint fix

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,20 @@ import ThreeModel from '@/components/ThreeModel';
 import WorkSection from '@/components/WorkSection';
 import Intro from '@/components/Intro';
 import AboutSection from './about';
+import Head from 'next/head';
 
 export default function Home({ allPostsData }) {
   return (
-    <ContentLayout>
-      <Container maxW='3xl'>
-        <WorkSection />
-        {/* <AboutSection /> */}
-      </Container>
-    </ContentLayout>
+    <>
+      <Head>
+        <title>Austin Hill.io</title>
+      </Head>
+      <ContentLayout>
+        <Container maxW='3xl'>
+          <WorkSection />
+          {/* <AboutSection /> */}
+        </Container>
+      </ContentLayout>
+    </>
   );
 }
